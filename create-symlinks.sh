e5296b0498c85f6486c414afe7b3e663a70ed9db
jordao <jordao@kth.se>
2019-12-10 09:57:23 +0100

    Added symlink maker

---
+++ create-symlinks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# get the script of the dir with this nice one-liner
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+
+# create the symlink for the config folder
+ln -s $SCRIPT_DIR/config $HOME/.config
+
+# create the symlink for the emacs folder
+ln -s $SCRIPT_DIR/emacs $HOME/.emacs.d
+
+
