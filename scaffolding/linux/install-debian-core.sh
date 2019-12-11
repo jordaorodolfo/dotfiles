9498c5553c96539b1ecc14ef11e2e2ca7b5b4e67
RJordao <jordao@kth.se>
2019-12-11 22:58:50 +0100

    Added media too

---
+++ scaffolding/linux/install-debian-core.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt install -y emacs \
+  neovim \
+  python3 \
+  python3-neovim \
+  git \
+  build-essential \
+  network-manager \
+  curl \
+  wget \
+  ranger \
+  fasd \
+  zsh \
+  acpi
