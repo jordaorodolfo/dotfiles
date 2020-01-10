2c26bf163c5735636c12a1e5e0cd5d8e0e3fdb07
Rodolfo Jordao <jordao@kth.se>
2020-01-10 15:27:44 +0100

    Added zsh and symlinks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -9,4 +9,7 @@ ln -s $SCRIPT_DIR/config $HOME/.config
 # create the symlink for the emacs folder
 ln -s $SCRIPT_DIR/emacs $HOME/.emacs.d
 
-
+# create symlinks for hidden stuff in home folder
+for f in $SCRIPT_DIR/home/hidden/* ; do
+  ln -s $(basename $SCRIPT_DIR/home/hidden/$f) ~/.$(basename $f)
+done
