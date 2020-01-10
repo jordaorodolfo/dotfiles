a80147435526dfda5816fb1d8be3054d76220a2e
Rodolfo Jordao <jordao@kth.se>
2020-01-10 15:20:41 +0100

    home now started and some config updats

--- emacs/init.el
+++ emacs/init.el
@@ -19,3 +19,17 @@
 (require 'org)
 (org-babel-load-file "~/.emacs.d/config.org")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ox-reveal yasnippet use-package projectile org-ref minizinc-mode magit lsp-mode god-mode flycheck counsel auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- scaffolding/linux/install-debian-core.sh
+++ scaffolding/linux/install-debian-core.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install -y emacs \
+sudo apt install -y emacs \
   neovim \
   python3 \
   python3-neovim \
@@ -12,3 +12,15 @@ apt install -y emacs \
   fasd \
   zsh \
   acpi
+
+# install oh my zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# install powerlevel9k
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.local/share/fonts/
+mv PowerlineSymbols.otf ~/.local/share/fonts
+fc-cache -vf ~/.local/share/fonts/
+mkdir -p ~/.config/fontconfig/conf.d/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
