a3f348355ea86a53818670c2a25ec47a67ab698f
Rodolfo Jordao <jordao@kth.se>
2020-01-17 15:31:08 +0100

    Added graphics

---
+++ scaffolding/linux/install-debian-graphics.sh
@@ -0,0 +1,7 @@
+sudo apt-get install i3
+sudo apt-get install i3blocks
+sudo apt-get install i3status
+sudo apt-get install rofi
+sudo apt-get install network-manager-gnome
+sudo apt-get install redshift
+sudo apt-get install redshift-gtk
