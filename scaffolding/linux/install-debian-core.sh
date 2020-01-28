73d0df48a083fe30abb973fe2c976cab94dbabf0
Rodolfo Jordao <jordao@kth.se>
2020-01-28 19:08:43 +0000

    forgotten do add

--- scaffolding/linux/install-debian-core.sh
+++ scaffolding/linux/install-debian-core.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-sudo apt install -y emacs \
-  neovim \
-  python3 \
-  python3-neovim \
-  git \
-  build-essential \
-  network-manager \
-  curl \
-  wget \
-  ranger \
-  fasd \
-  zsh \
-  acpi
+sudo apt install -y  emacs
+sudo apt install -y  neovim
+sudo apt install -y  python3
+sudo apt install -y  python3-neovim
+sudo apt install -y  git
+sudo apt install -y  build-essential
+sudo apt install -y  network-manager
+sudo apt install -y  curl
+sudo apt install -y  wget
+sudo apt install -y  ranger
+sudo apt install -y  fasd
+sudo apt install -y  zsh
+sudo apt install -y  acpi
 
 # install oh my zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # install powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mkdir -p ~/.local/share/fonts/
