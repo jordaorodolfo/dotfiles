11920c5dace33451ca5079f9cec78707d0c5b165
jordao <jordao@kth.se>
2020-03-04 11:29:26 +0100

    Added coc and scaff

---
+++ scaffolding/linux/install-debian-online.sh
@@ -0,0 +1,12 @@
+# possibly additional step
+sudo apt-get install -y apt-transport-https
+
+# Add the release PGP keys:
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+
+# Add the "stable" channel to your APT sources:
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
